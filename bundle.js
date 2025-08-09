72e152dca683b7ab2849556e4ead8e3219e1dd27
CamsAvis <CamsAvis@users.noreply.github.com>
2025-08-09 02:47:49 +0000

    deploy: da90d5ac31ec8e6ac11ad0fb3b5c7a4d9942745d

--- bundle.js
+++ bundle.js
@@ -1,7 +1,3 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script>
-
 window.addEventListener('DOMContentLoaded', () => {
     // recommended creators
     const selector = `
@@ -462,4 +458,3 @@ function addZoomer($zoomerTarget) {
 }
 
 
-</script>
