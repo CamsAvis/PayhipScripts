bfae230b4c6928c83d879f9b853decd836e60a71
CamsAvis <CamsAvis@users.noreply.github.com>
2025-08-10 20:56:13 +0000

    deploy: 805b9732b90b249cbc6b22ca01d28fd1b1a11d47

---
+++ individual/master-foldouts.js
@@ -0,0 +1,64 @@
+const isStart = ($element) => $element.text().trim().match(/\%\%FOLDOUT_START.*\%\%/);
+const isEnd = ($element) => $element.text().trim().match(/\%\%FOLDOUT_END.*\%\%/);
+
+const parseQuery = ($element) => {
+	let queryOutput = {}
+
+	let match;
+	const regex = /(?<key>[a-z0-9-]+)=(?<value>[a-z0-9-]+)/gi
+	while ((match = regex.exec($element.text())) !== null) {
+		queryOutput[key] = value;
+	}
+
+	return queryOutput;
+}
+
+const createFoldout = ($rootElement) => {
+		// main container
+		const $foldoutContainer = $("<div>")
+			.addClass("foldout-container")
+			.attr("data-folded-out", "true");
+
+		// header
+		const $foldoutHeader = $("<div>")
+			.addClass("foldout-header")
+			.on("click", function () {
+				const isFoldedOut = $foldoutContainer.attr("data-folded-out") === "true";
+				$foldoutContainer.attr("data-folded-out", (!isFoldedOut).toString());
+			});
+
+		// insert title
+		const $titleElement = $rootElement.next();
+		const $firstChildElement = $titleElement.next();
+		$titleElement.appendTo($foldoutHeader);
+
+		$rootElement.html("").append(
+			$foldoutHeader, $foldoutContainer
+		)
+
+		let $current = $firstChildElement;
+		while ($current.length && !isEnd($current)) {
+			if(isStart($current)) {
+				createFoldout($current);
+				continue;
+			}
+
+			const $next = $current.next();
+			$current.appendTo($foldoutContainer);
+			$current = $next;
+		}
+
+		if(isEnd($current)) {
+			$current.remove();
+		}
+}
+
+$(document).ready(function () {
+	$(".product-description > *").each((_, element) => {
+			const $rootElement = $(element);
+			if (!isStart($rootElement)) {
+				return;
+			}
+			createFoldout($rootElement);
+	});
+});
