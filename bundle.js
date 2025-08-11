fcaab9f3b66f47ae7cae9b2b49b331e2372a58b0
CamsAvis <CamsAvis@users.noreply.github.com>
2025-08-11 16:48:44 +0000

    deploy: 1fcfd9502c6641fe43596009b0fdd4725544992f

--- bundle.js
+++ bundle.js
@@ -459,12 +459,16 @@ const createFoldout = ($rootElement) => {
 		const query = parseQuery($rootElement);
 		const foldedOutByDefault = ('folded-out' in query) && (query['folded-out'] === "true");
 	
-		const FOLDOUT_TRANSITION_DURATION = (('duration' in query) && query['duration']) ?? "500ms";
-		// main container
+		const FOLDOUT_TRANSITION_DURATION_STR = (('duration' in query) && query['duration']) ?? "500ms";
+
+		const reResults = FOLDOUT_TRANSITION_DURATION_STR.match(/(?<time>[0-9]*)(?<unit>\w+)/);
+		const animDurationNumber = parseInt(reResults?.groups?.time || "500");
+		const animDurationUnit = reResults?.groups?.unit || "ms";
+
 		const $foldoutContainer = $("<div>")
 			.addClass("foldout-container")
 			.attr("data-folded-out", foldedOutByDefault.toString())
-			.css('--anim-foldout-dura', FOLDOUT_TRANSITION_DURATION);
+			.css('--anim-foldout-dura', `${animDurationNumber}${animDurationUnit}`);
 
 		// header
 		const $foldoutHeader = $("<div>")
@@ -482,7 +486,7 @@ const createFoldout = ($rootElement) => {
 						$foldoutContainer.css({ pointerEvents: "all" });
 						$foldoutContainer.attr("data-folded-out", (!isFoldedOut).toString());
 						$foldoutContainer.removeClass(animClassName);
-					}, FOLDOUT_TRANSITION_DURATION);
+					}, animDurationNumber);
 			});
 
 		// insert title
