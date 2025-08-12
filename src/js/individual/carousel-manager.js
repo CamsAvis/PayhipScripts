12fd4559fe71ae24d35d86d555f241d9844c8cf3
CamsAvis <samspwrsd@gmail.com>
2025-08-12 18:58:02 -0400

    fixed function not using 'this'

--- src/js/individual/carousel-manager.js
+++ src/js/individual/carousel-manager.js
@@ -165,7 +165,7 @@ class CarouselWrapper {
 
 		let carouselTimeoutObject = new CarouselTimeout([], [], 0, undefined, $root);
 		while ($current.length && !this.isCarouselEnd($current)) {
-			$current.find("img").each((_, img) => addCarousel($(img), $navGroup, imageTimeoutKey, navItemIdx));
+			$current.find("img").each((_, img) => this.addCarousel($(img), $navGroup, imageTimeoutKey, navItemIdx));
 			// $current.find("iframe").each((_, iframe) => addCarousel($iframe, $navGroup, imageTimeoutKey, navItemIdx));
 
 			const $next = $current.next();
